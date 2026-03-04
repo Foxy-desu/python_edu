0dc42cd5f81983f6402bc01c86244b79dd4e11a3
Alexandra Orluyk <iammeduza@gmail.com>
2026-03-04 21:38:09 +0300

    feat: add negative tests for tweet_limit check

--- tests.py
+++ tests.py
@@ -25,9 +25,32 @@ def test_tweet_limit_logic(tweet_str, tweet_limit, expected):
 def test_tweet_default_limit(tweet_str, expected):
     """Check the default limit is set and equals 280"""
     assert check_tweet_limit_excess(tweet_str) is expected, f'Failed: tweet_str={tweet_str}'
+
+
+@pytest.mark.xfail(reason="Validation of negative values isn't yet implemented")
+def test_tweet_limit_negative_val_fails():
+    """Checks if the function fails with ValueError on negative tweet_limit value"""
+    with pytest.raises(ValueError, match="Tweet limit cannot be negative"):
+        check_tweet_limit_excess( "some random tweet", -5)
+
+
+@pytest.mark.parametrize("tweet_limit", [
+    True,
+    "10",
+    None,
+    [280],
+    {280},
+    (280),
+    210.5,
+])
+@pytest.mark.xfail(reason="Validation of types is not implemented yet")
+def test_tweet_limit_type_check(tweet_limit):
+    """Checks if the function accepts different types for tweet limit"""
+    with pytest.raises(TypeError, match="Tweet value type can only be integer"):
+        check_tweet_limit_excess("some tweet string")
+
+
 # TODO
 
-# Лимит = 0,1
-# Лимит = другие типы данных помимо int
 # твит_стринг - пустой
 # твит_стринг - не строка
